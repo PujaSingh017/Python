ddeb524b337f2eb3342a0d3f8bc612f912324c5f
pujasingh017 <pujasingh20005@gmail.com>
2022-10-03 22:25:02 +0530

    commit

---
+++ cal.py
@@ -0,0 +1,28 @@
+                                            #My First Calculator#    
+
+first=input("enter first number:")
+operator=input("+,-,*,/,%")
+second=input("enter second number:")
+first=int(first)
+second=int(second)
+
+ # Operations Are Below:
+
+if operator=="+":
+    print(first+second)
+
+elif operator=="-":
+    print (first-second)
+
+elif operator=="*" :
+    print(first*second)
+
+elif operator=="/":
+    print(first)/(second)
+
+elif operator=="%":
+    print(first%second)
+
+
+else:
+     print("invaild operation")
